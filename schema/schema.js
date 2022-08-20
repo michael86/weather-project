4181f583469d9f65e8a643bbd761c8a2c62e56f9
michael86 <michael8t6@gmail.com>
2022-08-20 11:39:08 +0100

    removed unrequired schemas

--- schema/schema.js
+++ schema/schema.js
@@ -1,24 +1,6 @@
-export const weatherSchema = joi.object({
-  lat: joi.number(),
-  lon: joi.number(),
-  date: joi.number(),
-  desc: joi.string(),
-  icon: joi.string(),
-  feelsLike: joi.number(),
-  humidity: joi.number(),
-  temp: joi.number(),
-  windStrength: joi.number(),
-  windDirection: joi.number(),
-});
-
 export const userSchema = joi.object({
   location: joi
     .string()
-    .regex(/[-&\/\\#,+()$~%.'":*?<>{}]/g, { invert: true }) //Check for special characters, invert true means they are not allowed.
+    .regex(/[-&\/\\#,+()$~%.'":*?<>{}]/, { invert: true }) //Check for special characters, invert true means they are not allowed.
     .required(),
-});
-
-export const locationSchema = joi.object({
-  lat: joi.number(),
-  lon: joi.number(),
 });
